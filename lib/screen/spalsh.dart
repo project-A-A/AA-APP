1bf1992a85de8810f6c27ede9903cff88e2470e2
heoeunseo <heoeunseong140@gmail.com>
2025-12-27 01:32:53 +0900

    feat: Login 페이지 UI 퍼블리싱

--- lib/screen/spalsh.dart
+++ lib/screen/spalsh.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../utils/utils.dart';
+
 class SpalshScreen extends StatefulWidget {
   const SpalshScreen({super.key});
 
@@ -14,30 +16,7 @@ class _SpalshScreenState extends State<SpalshScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/image/aa_logo.png'),
-            SizedBox(height: 16),
-            Text(
-              '아.아.',
-              style: TextStyle(
-                color: Color(0xff2D1810),
-                fontWeight: FontWeight.w900,
-                fontSize: 35,
-              ),
-            ),
-            SizedBox(height: 12),
-            Text(
-              '커피 한 잔의 여유로운 대화',
-              style: TextStyle(
-                color: Color(0xff62748E),
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-              ),
-            ),
-          ],
-        ),
+        child: Utils.logo()
       ),
     );
   }
